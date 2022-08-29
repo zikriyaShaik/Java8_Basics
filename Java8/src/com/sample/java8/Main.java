9b50450455c8d85f959b751622a166e05b41b084
ZIKRIYA_SHAIK <zikriya23@gmail.com>
2022-08-29 17:06:14 +0530

    initial commit

---
+++ Java8/src/com/sample/java8/Main.java
@@ -0,0 +1,11 @@
+package com.sample.java8;
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java8....:");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
